2e36c6624e17e45c4e02a782189c5599366b6ab8
yomna00 <yomna0917@gmail.com>
2025-07-13 19:22:53 +0300

    create contact page

--- src/Pages/Contact.jsx
+++ src/Pages/Contact.jsx
@@ -117,7 +117,7 @@ const Contact = () => {
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2436.739040992053!2d-0.12967878403271732!3d51.50721787963571!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x48761b33123ef4f9%3A0x8696dc0b77cdbe0b!2sLondon!5e0!3m2!1sen!2seg!4v1710000000000"
                 width="100%"
-                height="350"
+                height="100%"
                 style={{ border: "0" }}
                 allowFullScreen=""
                 loading="lazy"
