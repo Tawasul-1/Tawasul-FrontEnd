2c4a68ddacbd9e3f288e07a7bf3c12d60774feb4
yosifshaban6 <yosifshaban6@gmail.com>
2025-07-15 06:24:44 +0300

    Minor fixes: improve App initialization and update styles

--- src/App.jsx
+++ src/App.jsx
@@ -9,22 +9,25 @@ import {
 function App() {
   useEffect(() => {
     const getAuthToken = () => {
-      return localStorage.getItem("authToken");
+      const token = localStorage.getItem("authToken");
+      console.log("getAuthToken called, token:", token ? "Token exists" : "No token");
+      return token;
     };
 
     const onUnauthenticated = () => {
       console.log("User is unauthenticated");
+      // Clear localStorage and redirect to login
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("refreshToken");
+      localStorage.removeItem("user");
+      window.location.href = "/login";
     };
 
     setupRequestInterceptor(apiClient, getAuthToken);
     setupResponseInterceptor(apiClient, onUnauthenticated);
   }, []);
 
-  return (
-    <>
-      <AppRouter />
-    </>
-  );
+  return <AppRouter />;
 }
 
 export default App;
