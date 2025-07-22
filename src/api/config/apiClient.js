29a781c13e63331e8f12341e6b7147d3a1f4af37
yosifshaban6 <yosifshaban6@gmail.com>
2025-07-22 04:43:43 +0300

    fix: allow FormData file upload by removing global Content-Type in Axios config

--- src/api/config/apiClient.js
+++ src/api/config/apiClient.js
@@ -2,9 +2,21 @@ import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:8000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  // Do not set Content-Type globally here
+});
+
+// Add a request interceptor to set Content-Type only for non-FormData
+apiClient.interceptors.request.use((config) => {
+  if (config.data instanceof FormData) {
+    // Let Axios set the Content-Type (multipart/form-data with boundary)
+    if (config.headers && config.headers["Content-Type"]) {
+      delete config.headers["Content-Type"];
+    }
+  } else {
+    config.headers = config.headers || {};
+    config.headers["Content-Type"] = "application/json";
+  }
+  return config;
 });
 
 export default apiClient;
